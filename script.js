7db0611d4da3c9d92069c5c403b945bc46c4bd67
nitish624 <nk3394841245@gmail.com>
2026-02-25 09:37:39 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function toggleMenu() {
     const menu = document.getElementById('side-menu');
         if (menu.style.transform === "translateX(0px)") {
-            menu.style.transform = "translateX(-20%)";
+            menu.style.transform = "translateX(-100%)";
         }
         else {
             menu.style.transform =
