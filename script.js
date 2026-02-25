5b26328ef71041cdf5ff5f68b4fd885c3750d137
nitish624 <nk3394841245@gmail.com>
2026-02-25 09:35:51 +0530

    update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-
-    function toggleMenu() {
-        const menu = document.getElementById('side-menu');
+function toggleMenu() {
+    const menu = document.getElementById('side-menu');
         if (menu.style.transform === "translateX(0px)") {
-            menu.style.transform = "translateX(-100%)";
-        } else {
-            menu.style.transform = "translateX(0px)";
+            menu.style.transform = "translateX(-20%)";
+        }
+        else {
+            menu.style.transform =
+            "translateX(0px)";
         }
     }
+    
